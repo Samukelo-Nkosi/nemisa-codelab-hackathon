accb6c2da43eb68b6e22e1fd3546d0d00d0d157d
Samukelo.dev <knkosi812@gmail.com>
2026-08-16 00:52:41 +0200

    added-new-file-folder-module-01

---
+++ module-01-introduction-to-coding/exercises/vpl_demo.py
@@ -0,0 +1,17 @@
+# Exercise 1: VPL Demo
+# Write a Python program that does the following:
+# Asks the user to enter their age.
+# Checks if the user is 18 or older.
+# If yes, print: "You are an adult!"
+# Otherwise, print: "You are not yet an adult."
+
+print("-----------VPL DEMO----------")
+age = int(input("Enter your age: "))
+
+print("-----------------------------")
+if age >= 18:
+    print("You are an adult.")
+
+else:
+    print("You are not an adult!")    
+print("-------------END-------------")
