7252d2c96f32c47cd0c92bc6f2f4117288184580
Samukelo.dev <knkosi812@gmail.com>
2026-08-22 14:12:26 +0200

    modified:   module-01-introduction-to-coding/exercises/vpl_demo_1.py

--- module-01-introduction-to-coding/exercises/vpl_demo_1.py
+++ module-01-introduction-to-coding/exercises/vpl_demo_1.py
@@ -1,18 +1,14 @@
 #Type of work:  Individual work
 #Exercise 1: Practising Comments and Indentation
-
 #Write a Python program that does the following:
-
 #Ask the user for their favourite colour.
-
 #Prints a message that includes the colour.
-
-#You must use comments to explain what each part of your code does and include proper indentation. Here is an example of what your output should look like:
+#You must use comments to explain what each part of your code does and include proper indentation. 
+#Here is an example of what your output should look like:
 
 #What is your favorite color? Blue
-
 #Your favorite color is Blue!
 
 favourite_colour = input("What is your favourite colour? : ")
 
-print("Your favourite colour is {favourite_colour}")
+print("Your favourite colour is " + favourite_colour + "!")
