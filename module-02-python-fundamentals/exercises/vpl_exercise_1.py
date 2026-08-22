ee292a97d937e4c949fdfedadf069646a0b51639
Samukelo.dev <knkosi812@gmail.com>
2026-08-22 15:22:08 +0200

    new file:   module-02-python-fundamentals/

---
+++ module-02-python-fundamentals/exercises/vpl_exercise_1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Let’s write your first complex code! 
+# Write a program that asks for your favourite colour and age, 
+# then prints a message combining both inputs.
+
+favourite_colour = input("What is your favorite colour? : ")
+age = input("Age: ")
+
+print("Your favorite colour is " + favourite_colour + " and you are " + age + " years old! ")
